50cc3a62bf2f67ff578170418869fafe31db115e
CasualAutopsy <Casual_Autopsy@outlook.com>
2026-01-20 18:26:56 -0500

    Add: API Function `sendEmbeddingRequest()`

--- src/api.js
+++ src/api.js
@@ -1,13 +1,17 @@
 import {extension_settings} from '../../../../extensions.js';
 
-/*
 export async function sendEmbeddingRequest(args, value) {
     const url = `${extension_settings.kcpplibs.url}/api/extra/embeddings`;
 
+    let docs = value;
+    if (typeof docs !== 'string') { // Parse list if un-named arg isn't a string
+        docs = JSON.parse(docs);
+    }
+
     const payload = {
         "model": "kcpp",
-        "input": value,
-        "truncate": args.truncate
+        "input": docs,
+        "truncate": args.truncate_input
     };
 
     const header = {
@@ -23,12 +27,23 @@ export async function sendEmbeddingRequest(args, value) {
             body: JSON.stringify(payload)
         });
 
+        // Retrieve the embedding info.
+        let result = await response.json();
+
+        if (args.vectors) { // If enabled, then only retrieve the vectors
+            result = result["data"];
+
+            let vectors = [];
+            result.forEach(function(vec) {
+                vectors.push(vec["embedding"])
+            });
+            return vectors;
+        }
         return await response.json();
     } catch (e) {
         console.error('KCppLibs:',e);
     }
 }
-*/
 
 
 
